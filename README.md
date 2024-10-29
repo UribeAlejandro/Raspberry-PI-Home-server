ebb3618072f57195809341d763f5e664bd22c3e3
Alejandro Uribe <jauriberamirez@gmail.com>
2024-10-28 22:06:47 -0300

    Added installation file with sample dockerfile and README

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Raspberry-PI-Home-server
+# Raspberry Pi - Home Server
+
+This repository contains the configuration files and scripts for my Raspberry Pi home server.
+
+## Installation
+
+1. Install Raspbian on the Raspberry Pi.
+2. Clone this repository to the Raspberry Pi.
+
+Install the required packages:
+
+```bash
+chmod +x install.sh
+sh install.sh
+```
+
+Wait for the Raspberry Pi to reboot.
+
+Clone this repository to the Raspberry Pi:
+
+```bash
+git clone https://github.com/UribeAlejandro/Raspberry-PI-Home-server.git
+```
+
+## Secrets
+
+Create a `.env` file in the root of the repository following the `.env.template` file.
+
+## Usage
+
+Run `docker-compose up -d` to start the services.
---
+++ init.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# More info on the Docker installation can be found at https://docs.docker.com/engine/install/debian/
+echo "Updating the package list" && \
+sudo apt-get update && \
+sudo apt-get install -y \
+  git \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg2 \
+  software-properties-common \
+  nano \
+  fail2ban \
+  ntfs-3g \
+&& \
+echo "Adding Docker GPG key" && \
+
+curl curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc && \
+sudo chmod a+r /etc/apt/keyrings/docker.asc && \
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null &&\
+
+echo "Installing Docker packages" && \
+sudo apt-get update && \
+sudo apt-get install -y --no-install-recommends docker-ce docker-compose && \
+
+echo "Adding the current user to the docker group" && \
+sudo usermod -a -G docker '$USER' && \
+
+echo "Rebooting the system" && \
+sleep 3 && \
+sudo reboot
