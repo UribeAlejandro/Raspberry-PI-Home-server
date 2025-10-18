0b8ae2898f566dd7ded81d7f8cc513ca18e10db6
Alejandro Uribe <jauriberamirez@gmail.com>
2025-10-18 20:26:55 -0300

    searxng secret

--- init_env.sh
+++ init_env.sh
@@ -1,3 +1,4 @@
 echo "SPEEDTEST_TRACKER_APP_KEY=base64:$(openssl rand -base64 32 | tr -d '\n')" >> .env && \
 echo "MYSQL_ROOT_PASSWORD=$(openssl rand -base64 36 | tr -d '\n')" >> .env && \
-echo "MYSQL_PASSWORD=$(openssl rand -base64 36 | tr -d '\n')" >> .env
+echo "MYSQL_PASSWORD=$(openssl rand -base64 36 | tr -d '\n')" >> .env && \
+echo "SEARXNG_SECRET=$(openssl rand -base64 32 | tr -d '\n')" >> .env
