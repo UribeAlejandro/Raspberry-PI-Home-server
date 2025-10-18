5d18155d6f8d54a91d9a73e62dfdf339a9068925
Alejandro Uribe <jauriberamirez@gmail.com>
2025-10-18 20:18:49 -0300

    Authentik removal (#25)

--- init_env.sh
+++ init_env.sh
@@ -1,5 +1,3 @@
-echo "PG_PASS=$(openssl rand -base64 36 | tr -d '\n')" >> .env && \
-echo "AUTHENTIK_SECRET_KEY=$(openssl rand -base64 60 | tr -d '\n')" >> .env && \
 echo "SPEEDTEST_TRACKER_APP_KEY=base64:$(openssl rand -base64 32 | tr -d '\n')" >> .env && \
 echo "MYSQL_ROOT_PASSWORD=$(openssl rand -base64 36 | tr -d '\n')" >> .env && \
 echo "MYSQL_PASSWORD=$(openssl rand -base64 36 | tr -d '\n')" >> .env
