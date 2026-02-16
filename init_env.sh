edabbab2b418bede72dbd97d867f0a9dbbb54184
Alejandro Uribe <jauriberamirez@gmail.com>
2026-02-16 16:25:14 -0300

    cleanup

--- init_env.sh
+++
@@ -1,3 +0,0 @@
-echo "SPEEDTEST_TRACKER_APP_KEY=base64:$(openssl rand -base64 32 | tr -d '\n')" >> .env && \
-echo "MYSQL_ROOT_PASSWORD=$(openssl rand -base64 36 | tr -d '\n')" >> .env && \
-echo "MYSQL_PASSWORD=$(openssl rand -base64 36 | tr -d '\n')" >> .env
